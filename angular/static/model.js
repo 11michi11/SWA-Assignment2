27445433a0617bc9199008d11ac99352d9cdbc54
Michal <11michi11@gmail.com>
2020-11-06 11:54:49 +0100

    This is so bad

--- angular/static/model.js
+++ angular/static/model.js
@@ -1,20 +1,75 @@
-const model = (persons, employees) => {
-    const employeeMap = {}
-    employees.forEach(e => employeeMap[e.employeeId] = e)
+const model = (place,
+               temperature,
+               precipitation,
+               wind,
+               cloud,
+               temperaturePrediction,
+               precipitationPrediction,
+               windPrediction,
+               cloudPrediction,
+               filter = () => true
+) => {
 
-    const personData = () => persons
-        .map(p => ({ ...p, ...employeeMap[p.employeeId]}))
+    const temperatureData = () => temperature.filter(filter)
+    const precipitationData = () => precipitation.filter(filter)
+    const windData = () => wind.filter(filter)
+    const cloudData = () => cloud.filter(filter)
+    const temperaturePredictionData = () => temperaturePrediction.filter(filter)
+    const precipitationPredictionData = () => precipitationPrediction.filter(filter)
+    const windPredictionData = () => windPrediction.filter(filter)
+    const cloudPredictionData = () => cloudPrediction.filter(filter)
 
-    const updatePerson = p => {
-        let i = persons.findIndex(pp => pp.id == p.id)
-        persons[i] = p
+    const filtered = filter => model(
+        place,
+        temperatureData,
+        precipitationData,
+        windData,
+        cloudData,
+        temperaturePredictionData,
+        precipitationPredictionData,
+        windPredictionData,
+        cloudPredictionData,
+        filter
+    )
+
+    const all = () => model(
+        place,
+        temperature,
+        precipitation,
+        wind,
+        cloud,
+        temperaturePrediction,
+        precipitationPrediction,
+        windPrediction,
+        cloudPrediction
+    )
+
+    const getLastMeasurement = () => model(
+        place,
+        temperature[temperature.length - 1],
+        precipitation[precipitation.length - 1],
+        wind[wind.length - 1],
+        cloud[cloud.length - 1],
+        temperaturePrediction,
+        precipitationPrediction,
+        windPrediction,
+        cloudPrediction
+    )
+
+    return {
+        place,
+        temperatureData,
+        precipitationData,
+        windData,
+        cloudData,
+        temperaturePredictionData,
+        precipitationPredictionData,
+        windPredictionData,
+        cloudPredictionData,
+        filtered,
+        all,
+        getLastMeasurement
     }
-    const addEmployee = e => {
-        employees.push(e)
-        employeeMap[e.employeeId] = e
-    }
-
-    return { personData, updatePerson, addEmployee }
 }
 
 export default model
